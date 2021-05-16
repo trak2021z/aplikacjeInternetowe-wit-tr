f539361c528f61d4e09edf1fa5e49dd013eb7009
Jakub Witoń <jakubwiton22@gmail.com>
2021-05-16 08:56:19 +0200

    Initial commit

---
+++ src/main/java/com/witon/jakub/ai/AplikacjeApplication.java
@@ -0,0 +1,13 @@
+package com.witon.jakub.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplikacjeApplication.class, args);
+    }
+
+}
